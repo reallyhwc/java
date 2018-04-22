9b30d72f4125fe84a122d375def7e11dc991b253
reallyhwc <33109924+reallyhwc@users.noreply.github.com>
2018-04-22 10:27:03 +0800

    p503同时启动两个线程

---
+++ headfirst_java_p503/RunThreads.java
@@ -0,0 +1,21 @@
+
+public class RunThreads implements Runnable {
+	public static void main(String[] args) {
+		RunThreads runner = new RunThreads();
+
+		//创建两个线程
+		Thread alpha = new Thread(runner);
+		Thread beta = new Thread(runner);
+		alpha.setName("Alpha thread");
+		beta.setName("		Beta thread");
+		alpha.start();
+		beta.start();
+	}
+
+	public void run() {
+		for (int i = 0; i < 100; i++) {
+			String threadName = Thread.currentThread().getName();
+			System.out.println(threadName + " is running");
+		}
+	}
+}
